08a3863ee6278ae1cf30c755f99d25f0b8cf1876
fritzgt <tuanis@live.com>
2019-07-11 09:24:26 -0400

    completed edit functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 export const NEW_TODO = "NEW_TODO";
 export const TOGGLE_COMPLETE = "TOGGLE_COMPLETE";
 export const DELETE_ITEM = "DELETE_ITEM";
+export const EDIT_ITEM = "EDIT_ITEM";
+export const UPDATE_TODO = "UPDATE_TODO";
 
 export function newTodo(newItem) {
   //   console.log("Action.js:", newItem);
@@ -22,5 +24,23 @@ export function deleteItem(index) {
   return {
     type: DELETE_ITEM,
     payload: index
+  };
+}
+
+//toggle edit mode to update
+export function editItem(index) {
+  console.log("Edit the following index:", index);
+  return {
+    type: EDIT_ITEM,
+    payload: index
+  };
+}
+//update todo by passing the new data
+export function updateTodo(data, index) {
+  console.log("Update data", data);
+  return {
+    type: UPDATE_TODO,
+    payload: data,
+    index: index
   };
 }
